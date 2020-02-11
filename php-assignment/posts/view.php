966017f8df9a792e0a07425bdb14a0147ad8f8a3
adilcse <adil.cs.work@gmail.com>
2020-02-11 18:33:53 +0530

    1101:view Created

--- php-assignment/posts/view.php
+++ php-assignment/posts/view.php
@@ -4,7 +4,8 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta http-equiv="X-UA-Compatible" content="ie=edge">
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-		<link rel="stylesheet" href="profileStyle.css">
+        <link rel="stylesheet" href="view.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 		<title>profile</title>
 		<?php
 			session_start();
@@ -12,19 +13,64 @@
 		?>
 	</head>
 	<body>
-		<?php include("../header/header.php");
-            include('dbConnectpdo.php');
+        <?php 
+            include("../header/header.php");
+          //  include('dbConnectpdo.php');
+            class viewPosts{
+               private $post= array(
+                    array("title"=>"Card Title","body"=>"Some quick example text to build on the card title and make up the bulk of the card's content."),
+                    array("title"=>"Card Title","body"=>"Some quick example text to build on the card title and make up the bulk of the card's content."),
+                    array("title"=>"Card Title","body"=>"Some quick example text to build on the card title and make up the bulk of the card's content.")
+                );
+
+                public function showPosts(){
+                    $post = $this->post;
+                    foreach($post as $value){
+                        echo '
+                        <div class="card ">
+                        <div class="card-body">
+                            <h5 class="card-title">'.$value["title"].'</h5>
+                           
+                            <p class="card-text">'.$value["body"].'</p>
+                            <h3><i onclick="myFunction(this)" class="fa fa-thumbs-up"></i> </h3> 
+                            <div class="form-group">
+                            <label for="comment">Comment</label>
+                            <textarea  class="form-control" id="comment" name="comment" row="2"></textarea>
+                            <button type="button" class="btn btn-secondary mt-2">Comment</button>
+                        </div>
+                        </div>
+                     </div>
+                   ' ;
+                    }
+                   
+
+                }
+            }
+        ?>
+         <div class ="container">
+         <div class="card post">
+            <div class="form-group">
+                <label for="username">Post Title</label>
+                <input type="text" class="form-control" id="title" name="title" >
+            </div>
+            <div class="form-group">
+                <label for="post">POST</label>
+                <textarea class="form-control" id="post" rows="3"></textarea>
+            </div>
+            <div class="mt-3">
+            <button type="button" class="btn btn-primary">Post</button>
+            <button type="button" class="btn btn-primary">Clear</button> 
+        </div> 
+        </div>
+        <?php
+            $post = new viewPosts();
+            $post->showPosts(); 
             ?>
-            <div class ="container">
-                <div class="card" >
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="card-link">Card link</a>
-                    <a href="#" class="card-link">Another link</a>
-                </div>
-                </div>
-                
+         </div>
+         <script>
+             function myFunction(x) {
+  x.classList.toggle("fa-thumbs-down");
+}
+         </script>    
     </body>
 </html>
--- php-assignment/profiledb/dbConnectpdo.php
+++ php-assignment/profiledb/dbConnectpdo.php
@@ -1,18 +1,14 @@
-
 <?php 
-    class DataBaseConnecter { 
-        private $servername = "myawsdatabase.cgciww58dmdb.us-east-2.rds.amazonaws.com";
-        private $username = "local";
-        private $password = "mindfire";
-        private $db = "php_profile";
+// DataBaseConnecter class have all the method using database
+include_once("../constants/database.php");
+    class DataBaseConnecter extends Database { 
         private static $obj;			
         private $conn;                     
         private final function __construct() { 
             try{
                 $this->conn = new PDO("mysql:host=$this->servername;dbname=$this->db", $this->username, $this->password);
                 $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                $this->conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-              
+                $this->conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);         
             }   
             catch(PDOException $e)
             {
@@ -32,144 +28,86 @@
         public function getConnect() {
             return $this->conn; 
         } 
-        public function selectFromMysql($table,$columns=['*'],$condition=["1"=>"1"]){
-            $col = implode(', ', $columns); 
+        //convert array to string for sql query.
+        private function getColumnString($col){
+            return implode(', ', $col); 
+        }
+        // convert array to string for sql
+        private function getConditions($condition,$sep="AND"){
             $con='';
             $val =[];
             foreach($condition as $key=>$value){
-                $con .= $key." = ? "." AND ";
+                $con .= $key." = ? ".$sep." ";
                 array_push($val,$value);            
             }
-            $con = trim($con," AND ");
-           
-
+            $con = trim($con,$sep." ");
+            return array($val,$con);
+        }
+        //run sql
+        private function runSqlCommand($sql,$val,$type="select"){
             try{
-               
-                $sql="SELECT $col FROM $table WHERE $con ;";
                 $stmt = $this->conn->prepare($sql);  
-                $stmt->execute($val);
-                
-               return ($stmt->fetchAll());
-              
-               
+                $stmt->execute($val); 
+                if($type === "select")
+                     return ($stmt->fetchAll());
+                else
+                    return true;     
             }catch(PDOException $e)
             {
-           // echo $sql . "<br>" . $e->getMessage();
             return false;
             }
         }
-        public function selectFromMysqlJoin($table,$columns,$joinType,$joinTable,$onCondition,$whereCondition=["true"=>"true"],$lmt=''){
-            $col = implode(', ', $columns); 
-            $val =[];
-            $con = '';
-            foreach($whereCondition as $key=>$value){
-                    $con .= $key." = ? "." AND "; 
-                array_push($val,$value);  
-            }
-            $con = trim($con," AND");
-            $onCon='';
-            foreach($onCondition as $key=>$value){
-                    $onCon .= $key." = ".$value." AND ";  
-                    array_push($val,$value);
-            }
-           $onCon= trim($onCon," AND ");
-            $sql = "SELECT $col FROM $table $joinType $joinTable ON $onCon  WHERE $con $lmt";
-            try{
-                $stmt = $this->conn->prepare($sql);
-                $stmt->execute($val);  
-                return ($stmt->fetchAll()); 
-            }catch(PDOException $e)
-            {
-          //  echo $sql . "<br>" . $e->getMessage();
-            return false;
-            }
-            
+        //to select any column from any table by giving condition
+        public function selectFromMysql($table,$columns=['*'],$condition=["1"=>"1"]){
+            $col = $this->getColumnString($columns);
+            $conditions=$this->getConditions($condition);
+            $val = $conditions[0];
+            $con = $conditions[1];
+            $sql="SELECT $col FROM $table WHERE $con ;";
+            return $this->runSqlCommand($sql,$val);
         }
+       
         public function updateMysql($table,$setColumns,$condition)
         {
-           
-            $val=[];
-            $col = '';
-            foreach($setColumns as $key=>$value){
-                $col .= $key." = ? "." , ";    
-                array_push($val,$value);    
-            }
-            $col = trim($col,", ");
-           
-
-           
-            $con = '';
-            foreach($condition as $key=>$value){  
-                $con .= $key." = ? AND";      
-                array_push($val,$value);   
-            }
-            $con =  trim($con,'AND');
-           
+            //generate comma separated string for column name
+            $columns=$this->getConditions($setColumns,",");
+            $val1 = $columns[0];
+            $col = $columns[1];
+            //generate and separated string for condition
+           $conditions = $this->getConditions($condition);
+           $con = $conditions[1];
+           $val2 = $conditions[0];
+           $val =  array_merge($val1,$val2);
             $sql="UPDATE $table SET $col WHERE $con;";
-            echo $sql.'\n';
-            try{
-                $stmt = $this->conn->prepare($sql);
-                $stmt->execute($val);  
-                return (true);  
-            }catch(PDOException $e)
-            {
-             
-
-          //  echo $sql . "<br>" . $e->getMessage();
-            return false;
-            }
+            return $this->runSqlCommand($sql,$val,"update");
+            
         }
         public function insertIntoMysql($table,$columns,$values)
         {
-            $col=implode(', ', $columns);  
+            $col = $this->getColumnString($columns);
+            //generate sql with ? for pdo insertion
             $q=[];
             foreach($values as $v){
                 array_push($q,"?");
             }
-            $val=implode(', ', $q);
+            $val= $this->getColumnString($q);
             $sql_insert="INSERT INTO $table($col) VALUES($val)";
-            try{
-                $stmt_insert=$this->conn->prepare($sql_insert);
-                $stmt_insert->execute($values);  
-                
-                return true;
-            }catch(PDOException $e)
-            {
-          //  echo $sql . "<br>" . $e->getMessage();
-            return false;
-            }
+            return $this->runSqlCommand($sql_insert,$values,"insert");
+           
 
         }
         public function deleteFromMysql($table,$condition)
         {
-            $con='';
-           
-            $val =[];
-            foreach($condition as $key=>$value){
-             
-                    $con .= $key." = ? AND ";
-                   
-                array_push($val,$value);
-                
-            }
-            $con = trim($con,"AND ");
+            $conditions=$this->getConditions($condition);
+            $con=$conditions[1]; 
+            $val =$conditions[0];
             $sql_delete="DELETE FROM $table WHERE $con"; 
-            try{
-               
-                $stmt = $this->conn->prepare($sql_delete);  
-                $stmt->execute($val);
-                return true;
-            }catch(PDOException $e)
-            {
-            
-            return false;
-            }
-
+            return $this->runSqlCommand($sql_delete,$val,"delete");
         }
-        public function test(){
-          $res =  $this->selectFromMysql("users",["first_name"],["age"=>26,"age"=>23]);
-            var_dump($res);
-        }
+        // public function test(){
+        //   $res =  $this->selectFromMysql("users",["first_name"],["age"=>26,"age"=>23]);
+        //     var_dump($res);
+        // }
        
     }
     $DBConnector = DataBaseConnecter::getInstance(); 
